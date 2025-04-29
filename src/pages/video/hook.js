f4aa524bd86f558222db50e9e37f5cccf8a86979
yumin1209 <yumin991209@naver.com>
2025-04-29 10:30:38 +0900

    📦Chore: nginx 설정 추가

--- src/pages/video/hook.js
+++ src/pages/video/hook.js
@@ -4,8 +4,8 @@ export const getVideoData = async (videoId) => {
   try {
     console.log('getVideoData 호출됨', videoId);
     const [videoResponse, recommendResponse] = await Promise.all([
-      axios.get(`/videos?videoId=${videoId}`),
-      axios.get(`/videos/recomm?videoId=${videoId}`),
+      axios.get(`/api/videos?videoId=${videoId}`),
+      axios.get(`/api/videos/recomm?videoId=${videoId}`),
     ]);
 
     console.log('videoResponse.data:', videoResponse.data);
