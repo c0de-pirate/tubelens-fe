11ba0d8aa79e8f8dec296dbcb9ca28920e22eda8
Azamman327 <wjsdbdud327@naver.com>
2025-04-29 09:34:23 +0900

    📦Chore: nginx 연동을 위한 vite.config.js 수정

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    proxy: {
-      '/videos': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
-      '/youtube': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      }
-    }
+    host: true,
   },
 });
